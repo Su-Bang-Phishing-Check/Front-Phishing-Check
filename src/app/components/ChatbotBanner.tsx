a987540090cddbc66641fca5609671138051d5bc
hykim <joywithjoy1203@gmail.com>
2025-07-17 10:54:44 +0900

    feat: 챗봇 배너 추가 작업

---
+++ src/app/components/ChatbotBanner.tsx
@@ -0,0 +1,15 @@
+"use client";
+import ChatStepIcon from "./ChatStepIcon";
+
+export default function ChatbotBanner() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-white border-3 border-[#CEE3FF] p-4 rounded-lg shadow-sm text-black w-[450px] h-[200px]">
+      <h1 className="text-lg font-semibold">
+        상황선택 → 요구사항선택 → 위험도 확인
+      </h1>
+      <div>
+        <ChatStepIcon />
+      </div>
+    </div>
+  );
+}

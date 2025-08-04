864d6a421727ab821c347c3908fd75a4eae032b4
DESKTOP-L3VIVLD\hy_glory <joywithjoy1203@gmail.com>
2025-08-05 02:47:54 +0900

    contact-book: 이미지 처리 제외

--- src/app/contact-book/components/ContactList.tsx
+++ src/app/contact-book/components/ContactList.tsx
@@ -1,14 +1,21 @@
-import { JSX } from "react";
-import { ContactType } from "../page";
+import { JSX } from 'react';
+import { ContactType } from '../page';
 
-const ContactList = ({ name, phone, image }: ContactType): JSX.Element => {
+const ContactList = ({ name, phone }: ContactType): JSX.Element => {
   return (
     <div
-      className="w-full flex items-center p-4 bg-white rounded-lg text-black shadow-sm
-    text-[1.125rem] md:text-[1.25rem] select-none"
+      className="flex flex-col md:flex-row items-center p-3 md:p-4 bg-white rounded-lg text-black shadow-sm
+    text-[1rem] md:text-[1.25rem] select-none h-[70px] md:h-[90px]"
     >
-      <p className="flex-1 font-bold">{name} |</p>
-      <p className="flex-1 font-medium">{phone}</p>
+      <p className="w-full md:w-3/5 font-bold">{name}</p>
+      <p className="w-full md:w-2/5 flex md:flex-col font-medium md:hidden">
+        {phone.join(', ')}
+      </p>
+      <p className="hidden md:inline-block w-full md:w-2/5 flex md:flex-col font-medium">
+        {phone.map((p) => (
+          <p key={p}>{p}</p>
+        ))}
+      </p>
     </div>
   );
 };
--- src/app/contact-book/page.tsx
+++ src/app/contact-book/page.tsx
@@ -1,17 +1,22 @@
-import ContactList from "./components/ContactList";
-import list from "./data/list.json";
+import ContactList from './components/ContactList';
+import list from './data/list.json';
+
 export interface ContactType {
   name: string;
-  phone: string;
+  phone: string[];
   image?: string;
 }
 
 const ContactBookPage = () => {
   return (
     <div className="min-h-screen bg-[#f7fbff] p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4">
         {list.map((bank: ContactType) => (
-          <ContactList name={bank.name} phone={bank.phone} />
+          <ContactList
+            key={bank.name}
+            name={bank.name}
+            phone={bank.phone}
+          />
         ))}
       </div>
     </div>

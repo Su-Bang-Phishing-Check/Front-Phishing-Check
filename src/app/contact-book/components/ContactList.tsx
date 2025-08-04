a06e051d3000dfa8f014014fca8a05282415e6b1
hykim <joywithjoy1203@gmail.com>
2025-08-04 20:54:55 +0900

    작업중

--- src/app/contact-book/components/ContactList.tsx
+++ src/app/contact-book/components/ContactList.tsx
@@ -1,21 +1,14 @@
 import { JSX } from "react";
+import { ContactType } from "../page";
 
-interface ContactProps {
-  id: number;
-  name: string;
-  phone: string;
-  image: string;
-}
-
-const ContactList = ({ id, name, phone, image }: ContactProps): JSX.Element => {
+const ContactList = ({ name, phone, image }: ContactType): JSX.Element => {
   return (
-    <div>
-      <img
-        src={image}
-        alt={`${name}'s profile`}
-        className="w-16 h-16 rounded-full"
-      />
-      <p>{phone}</p>
+    <div
+      className="w-full flex items-center p-4 bg-white rounded-lg text-black shadow-sm
+    text-[1.125rem] md:text-[1.25rem] select-none"
+    >
+      <p className="flex-1 font-bold">{name} |</p>
+      <p className="flex-1 font-medium">{phone}</p>
     </div>
   );
 };
--- src/app/contact-book/page.tsx
+++ src/app/contact-book/page.tsx
@@ -1,8 +1,19 @@
+import ContactList from "./components/ContactList";
+import list from "./data/list.json";
+export interface ContactType {
+  name: string;
+  phone: string;
+  image?: string;
+}
+
 const ContactBookPage = () => {
   return (
-    <div>
-      <h1>Contact Book</h1>
-      <p>Welcome to the contact book page!</p>
+    <div className="min-h-screen bg-[#f7fbff] p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {list.map((bank: ContactType) => (
+          <ContactList name={bank.name} phone={bank.phone} />
+        ))}
+      </div>
     </div>
   );
 };

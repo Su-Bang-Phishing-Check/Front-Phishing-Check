5ac04a62913af187f27db226c6eea579af705271
DESKTOP-L3VIVLD\hy_glory <joywithjoy1203@gmail.com>
2025-08-14 00:37:25 +0900

    헤더 높이 설정

--- src/app/Layout/Header.tsx
+++ src/app/Layout/Header.tsx
@@ -1,14 +1,17 @@
-"use client";
-import { useRouter } from "next/navigation";
-import DrawerRight from "./DrawerRight";
+'use client';
+import { useRouter } from 'next/navigation';
+import DrawerRight from './DrawerRight';
 
 export default function Header() {
   const router = useRouter();
 
   return (
-    <header className="flex items-center justify-between p-4 bg-blue-50 shadow-md">
+    <header
+      className="flex items-center justify-between px-4 
+    bg-blue-50 shadow-md h-14 md:h-20"
+    >
       <h1
-        onClick={() => router.push("/")}
+        onClick={() => router.push('/')}
         className="text-xl md:text-2xl font-bold text-gray-800 cursor-pointer"
       >
         피싱체크
